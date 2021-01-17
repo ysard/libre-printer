3488f1ec07baf9eb85432e5e746a3853fa7315f4
ysard <ysard@users.noreply.github.com>
2021-01-18 00:52:14 +0100

    Add copyright header

--- libreprinter/commons.py
+++ libreprinter/commons.py
@@ -1,3 +1,20 @@
+# Libreprinter is a software allowing to use the Centronics and serial printing
+# functions of vintage computers on modern equipement through a tiny hardware
+# interface.
+# Copyright (C) 2020-2021  Ysard
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Logger settings and project constants"""
 # Standard imports
 from logging.handlers import RotatingFileHandler
