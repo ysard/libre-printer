f029fad66c4852eea89e992cac74b6034a4176a8
ysard <ysard@users.noreply.github.com>
2022-06-19 04:02:36 +0200

    [tests][watchdog] Begin to support all watchdogs in 1 parametrized test

--- tests/test_jobs_to_printer_watchdog.py
+++ tests/test_jobs_to_printer_watchdog.py
@@ -21,11 +21,14 @@ Test only file detections & startups.
 """
 # Standard imports
 import time
+import pytest
 from unittest.mock import patch
 
 # Custom imports
 from libreprinter.file_handler import init_directories
 from libreprinter.jobs_to_printer_watchdog import setup_watchdog
+from libreprinter.pcl_to_pdf_watchdog import setup_pcl_watchdog
+from libreprinter.commons import PCL_CONVERTER
 
 # Import create dir fixture
 from .test_file_handler import temp_dir
@@ -39,26 +42,66 @@ def mock_on_closed(self, event):
     CATCHED_EVENTS.append(event)
 
 
+@pytest.mark.timeout(3)
 @patch("libreprinter.jobs_to_printer_watchdog.PdfTxtEventHandler.on_closed", mock_on_closed)
-def test_setup_watchdog(temp_dir):
+@patch("libreprinter.pcl_to_pdf_watchdog.PclEventHandler.on_closed", mock_on_closed)
+@pytest.mark.parametrize(
+    "watchdog, config, files_to_create, expected_file",
+    [
+        # Test the detection of a pdf file creation in /pdf
+        (
+            setup_watchdog,
+            {"misc": {"output_printer": None}},
+            ["pdf/aaa", "pdf/x.pdf"],
+            "pdf/x.pdf",
+        ),
+        # Test the detection of pcl file creation in /pcl
+        (
+            setup_pcl_watchdog,
+            {"misc": {"pcl_converter_path": PCL_CONVERTER}},
+            ["pcl/aaa", "pcl/x.pcl"],
+            "pdf/x.pdf",
+        ),
+    ],
+    ids=["jobs_to_printer_watchdog", "pcl_to_pdf_watchdog"],
+)
+def test_setup_watchdog(watchdog, config, files_to_create, expected_file, temp_dir):
     """Test the detection of pdf creation in /pdf
 
-    `on_closed` method is modified to show catched event instead of redirecting
-    pdf to a Cups printer.
+    `on_closed` method is modified to show catched event instead of doing things;
+    see :meth:`mock_on_closed`.
+
+    .. note:: Only the watchdog behaviors are tested here. What the it does
+        after the event detection is not tested here
+        (see :meth:`test_interface` instead).
+
+    :param watchdog: Function that setup a watchdog according to a given config
+    :param config: Config in the format returned by a Configuration Parser.
+        Not all settings are required for a specific watchdog.
+    :param files_to_create: List of files to create to wake-up the watchdog
+    :param expected_file: Expected file in files_to_create that should recognized
+        by the watchdog.
+    :param temp_dir: (fixture) Create temp directory
+    :type watchdog: <function>
+    :type config: <dict>
+    :type files_to_create: <list <str>>
+    :type expected_file: <str>
+    :type temp_dir: <str>
     """
     init_directories(temp_dir)
 
-    # on_closed = mock_on_closed
-    setup_watchdog({"misc": {"output_path": temp_dir, "output_printer": None}})
+    # Add temporary dir to config
+    config["misc"]["output_path"] = temp_dir
+    # Start watchdog
+    watchdog(config)
 
     # Only x.pdf should be catched
-    open(temp_dir + "pdf/aaa", "a").close()
-    open(temp_dir + "pdf/x.pdf", "a").close()
+    [open(temp_dir + filename, "a").close() for filename in files_to_create]
 
     while not CATCHED_EVENTS:
         time.sleep(0.5)
 
-    assert "pdf/x.pdf" in CATCHED_EVENTS[0].src_path
+    assert expected_file in CATCHED_EVENTS[0].src_path
 
     assert len(CATCHED_EVENTS) == 1
     print(CATCHED_EVENTS)
