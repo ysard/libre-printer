8b3c476777c65b4905b3f124a038280a0c4a8fc5
ysard <ysard@users.noreply.github.com>
2022-06-28 01:40:24 +0200

    [doc] Add mermaid plugin (not functional for now)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,6 +31,7 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxcontrib.mermaid',
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
