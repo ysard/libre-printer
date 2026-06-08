4028790cb95332d0d19e2890ec544fea5c649738
ysard <ysard@users.noreply.github.com>
2026-06-08 15:25:45 +0200

    [plugins] Add maintenance warning for tests if new decorators are added

--- libreprinter/plugins_handler.py
+++ libreprinter/plugins_handler.py
@@ -40,6 +40,9 @@ _PLUGINS = {}
 _CONFIGURERS = {}
 
 
+# WARNING: for each added decorator (function decorated), please update
+# :meth:`tests.test_plugins.handle_module_cache` function!
+
 def register(func):
     """Decorator for registering a new plugin"""
     package, _, plugin = func.__module__.rpartition(".")
